0df8c8b85cedad9d22d6dcbb2217c4f8433efb64
lancer07 <lancer07@126.com>
2017-08-18 14:40:37 +0800

    接口列表修改

--- app/tests/api/list.js
+++ app/tests/api/list.js
@@ -1,13 +1,14 @@
-var list = [
+const baseUrl = 'http://127.0.0.1:12345';
+const list = [
     {
         key : '登录',
         type : 'post',
-        url : 'http://127.0.0.1:12345/api/login'
+        url : baseUrl + '/api/login'
     },
     {
         key : '用户列表',
         type : 'get',
-        url : 'http://127.0.0.1:12345/api/user'
+        url : baseUrl + '/api/user'
     }
 ]
 
