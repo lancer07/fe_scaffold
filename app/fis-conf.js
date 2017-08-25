e6fa9b63cbedbab0f6358c3915d034cc99edc455
lancer07 <lancer07@126.com>
2017-08-25 11:17:38 +0800

    格式修改

--- app/fis-conf.js
+++ app/fis-conf.js
@@ -1,65 +1,80 @@
 var requirejs = require('./r.js');
+
 fis.match('::package', {
-  spriter: fis.plugin('csssprites'),
-  postpackager: fis.plugin('loader', {
-      resourceType: 'amd',
-      useInlineMap: true
-  })
+	spriter: fis.plugin('csssprites'),
+	postpackager: fis.plugin('loader', {
+		resourceType: 'amd',
+		useInlineMap: true
+	})
 });
 
 fis.match('*', {
-  useHash: false
+	useHash: false
 });
 
 fis.match('{components,pages}/*.less', {
-  parser: fis.plugin('less'),
-  rExt: '.css'
+	parser: fis.plugin('less'),
+	rExt: '.css'
 });
 
 fis.match('*.css', {
-  useSprite: true,
-  optimizer: fis.plugin('clean-css')
+	useSprite: true,
+	optimizer: fis.plugin('clean-css')
 });
 
 fis.match('res/images/*.png', {
-  optimizer: fis.plugin('png-compressor')
+	optimizer: fis.plugin('png-compressor')
 });
 
 fis.match('*.{html,tpl}', {
-  optimizer: fis.plugin('html-minifier',{
-    processConditionalComments : true
-  }),
-  useMap: true
+	optimizer: fis.plugin('html-minifier', {
+		processConditionalComments: true
+	}),
+	useMap: true
 });
 
 fis.media('prod').match('pages/**.js', {
-  postprocessor: function (content, file, settings) {
-      requirejs.optimize({
-          baseUrl: './',
-          name: file.subpathNoExt.slice(1),
-          out: '../dist/' + file.subpath,
-          //optimize: 'none'
-      }, function (resultText) {
-          console.log('requirejs.optimize:\n===================\n' + resultText);
-      });
-  }
+	postprocessor: function (content, file, settings) {
+		requirejs.optimize({
+			baseUrl: './',
+			name: file.subpathNoExt.slice(1),
+			out: '../dist/' + file.subpath,
+			//optimize: 'none'
+		}, function (resultText) {
+			console.log('requirejs.optimize:\n===================\n' + resultText);
+		});
+	}
 });
 
 fis.media('prod').match('*.{html,tpl}', {
-    optimizer: fis.plugin('html-minifier',{
-      processConditionalComments : true,
-      minifyJS : true,
-      trimCustomFragments : true
-    })
-})
+	optimizer: fis.plugin('html-minifier', {
+		processConditionalComments: true,
+		minifyJS: true,
+		trimCustomFragments: true
+	})
+});
+
+fis.config.set('settings.optimizer.uglify-js', {
+	compress: {
+		drop_console: true
+	},
+	output: {
+		beautify: false,
+		preamble: "/* uglified */"
+	}
+});
+fis.media('prod').match('*.js', {
+	optimizer: fis.plugin('uglify-js')
+});
+
 
 fis.set('project.ignore', [
-  'node_modules/**',
-  'res/less/**',
-  'res/vendor/**', // 打包时，忽略
-  'res/vue/**',
-  //'tests/**',
-  '.git/**'
-  //'fis-conf.js',
-  //'package.json'
+	'node_modules/**',
+	'res/less/**',
+	'res/vendor/**', // 打包时，忽略
+	'res/vue/**',
+	//'tests/**',
+	'.git/**'
+	//'fis-conf.js',
+	//'package.json'
 ]);
